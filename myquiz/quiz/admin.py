656a19a471e0e8f20ab68838eb40253acd1d260d
joystonbritto <82714311+joystonbritto@users.noreply.github.com>
2021-05-06 22:04:03 +0530

    Delete myquiz directory

--- myquiz/quiz/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from quiz.models import Quiz
-
-# Register your models here.
-class QuizAdmin(admin.ModelAdmin):
-	list_display = ('question',)
-
-
-admin.site.register(Quiz, QuizAdmin)
