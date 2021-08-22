daed0b959b090babc23adf44d828dfa827656818
joystonbritto <82714311+joystonbritto@users.noreply.github.com>
2021-08-22 09:37:57 +0530

    Delete School directory

--- School/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include"database.php";
-	session_start();
-	
-	$s="delete from class where CID={$_GET["id"]}";
-	$db->query($s);
-	echo "<script>window.open('add_class.php?mes=Data Deleted.','_self');</script>"
-?>
--- School/staff_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include"database.php";
-	session_start();
-	$s="delete from staff where TID={$_GET["id"]}";
-	$db->query($s);
-	echo"<script>window.open('view_staff.php','_self');</script>";
-
-?>
